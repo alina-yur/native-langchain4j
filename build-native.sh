e03b317686e3e01d564dbe4015d8dedefe682274
alina-yur <alina.yurenko@oracle.com>
2024-03-18 15:37:23 +0100

    add a build-native script, fix typos

---
+++ build-native.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+native-image -jar ./target/native-langchain4j-1.0-jar-with-dependencies.jar -o native-langchain4j -H:ConfigurationFileDirectories=./resources/META-INF/native-image -H:+AllowDeprecatedBuilderClassesOnImageClasspath
---
+++ run-jvm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ./target/native-langchain4j-1.0-jar-with-dependencies.jar "$@"
